6b8258d1f687d52afff7d5ce05f055690f667e01
Valentyn <valantine10010@gmail.com>
2019-02-24 14:51:29 +0200

    first add

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React  from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+     
+      </div>
+    );
+  }
+}
+
+export default App;
