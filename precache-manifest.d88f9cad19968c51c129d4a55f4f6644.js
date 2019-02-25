196fd2d3affc349e1d14fa0dcd3205219bcea85a
Valentyn <valantine10010@gmail.com>
2019-02-25 16:17:20 +0200

    Updates

---
+++ precache-manifest.d88f9cad19968c51c129d4a55f4f6644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e37f32075bee95c42f",
+    "url": "/tabs-react/static/js/runtime~main.08e37f32.js"
+  },
+  {
+    "revision": "24f626937c4f03858e59",
+    "url": "/tabs-react/static/js/main.24f62693.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/tabs-react/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "24f626937c4f03858e59",
+    "url": "/tabs-react/static/css/main.66136bb6.chunk.css"
+  },
+  {
+    "revision": "fdb8d58a6a0e62227278264ebe2183ef",
+    "url": "/tabs-react/index.html"
+  }
+];
